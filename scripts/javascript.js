b4ea61fa47d4e5e8d40083b1f6309c1096741d06
Macca1986 <matthew_mack86@hotmail.com>
2025-07-08 16:27:35 +0400

    Create the GameBoard object and functions
    Created the GameBoard object and functions to set the cell,
    reset the board, and get the board.

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,1 +1,24 @@
 console.log('hello world');
+
+const GameBoard = (function () {
+    let board = ["","","","","","","","",""];
+
+    const getBoard = function () {
+        return board;
+    }
+
+    const setCell = function (index, marker) {
+        if (board[index] == "") {
+            board[index] = marker;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    const reset = function () {
+        board = ["","","","","","","","",""];
+    }
+
+    return { getBoard, setCell, reset };
+})();
