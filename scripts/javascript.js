7fb46d2d4b88292fba4439554b02b9e7ca2e43d4
Macca1986 <matthew_mack86@hotmail.com>
2025-07-09 16:14:09 +0400

    Create the GameBoard object and functions
    Created the GameBoard object and functions to set the cell,
    reset the board, and get the board. Added logic for playing the game.

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -22,3 +22,88 @@ const GameBoard = (function () {
 
     return { getBoard, setCell, reset };
 })();
+
+const GameController = (function () {
+    // logic for handling player turns, checking for wins and ties, and switching turns
+    const player1 = createPlayer('Player 1', 'X');
+    const player2 = createPlayer('Player 2', 'O'); 
+    // console.log('players created');
+
+    // set initial player
+    let currentPlayer = player1;
+
+    const getCurrentPlayer = function () {
+        return currentPlayer;
+    }
+
+    const changePlayer = function() {
+        if (currentPlayer == player1) {
+            currentPlayer = player2;
+        } else {
+            currentPlayer = player1;
+        }
+    }
+
+    const checkWin = function(board, marker) {
+        // define winning combinations in an array
+        const winningCombos = [
+            [0,1,2], [3,4,5], [6,7,8], // horizontal rows
+            [0,3,6], [1,4,7], [2,5,8], // vertical rows
+            [0,4,8], [2,4,6] // diagonals
+        ];
+
+        // check the board for winning combos
+        return winningCombos.some(function(combo) {
+            return combo.every(function(index) {
+              return board[index] === marker;
+            });
+        });
+    };
+
+    const checkTie = function (board) {
+        // if anyone has won, return false
+        if (checkWin(board, 'X') || checkWin(board, 'O')) {
+            return false;
+        };
+        // if any cells are empty return false
+        for (let i = 0; i < board.length; i++) {
+            if (board[i] == '') {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    // logic to input player move
+    const processTurn = function (index) {
+        // update the game board based on chosen cell (index) if it is valid
+        if (GameBoard.setCell(index, currentPlayer.marker)) {
+            let currentBoard = GameBoard.getBoard();
+            // check for win before changing the player
+            if (checkWin(currentBoard, currentPlayer.marker) == true) {
+                console.log('game has been won');
+            }
+            // check for tie
+            if (checkTie(currentBoard) == true) {
+                console.log('game has been tied');
+            } else {
+                console.log('no tie');
+            }
+            // change the player
+            changePlayer();
+            console.log('next player will be: ' + currentPlayer.marker);
+        } else {
+            console.log('invalid selection');
+        };
+
+    }
+
+    return { getCurrentPlayer, processTurn };
+})();
+
+function createPlayer (name, marker) {
+    return { 
+        name, 
+        marker,
+    };
+}
